d968bb9fa1ceba8f26f9f19aeb6cd640fd2cac33
HaijunWei <whj929159021@hotmail.com>
2025-04-15 16:07:58 +0800

    补充VideoController方法

--- flutter_baijiayun/lib/src/video_player_controller.dart
+++ flutter_baijiayun/lib/src/video_player_controller.dart
@@ -101,12 +101,20 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     PlatformVideoPlayerControllerCreationParams params,
   )   : platform = PlatformVideoPlayerController(params),
         super(VideoPlayerValue(duration: Duration.zero)) {
+    _lifeCycleObserver = _VideoAppLifeCycleObserver(this);
+    _lifeCycleObserver.initialize();
     _eventSubscription = platform.videoEvents().listen(_eventListener, onError: _errorListener);
   }
 
   final PlatformVideoPlayerController platform;
 
   bool _isDisposed = false;
+  bool _autoPlay = false;
+  bool _backgroundPlay = false;
+  bool get backgroundPlay => _backgroundPlay;
+  Duration? _initializedPosition;
+
+  late _VideoAppLifeCycleObserver _lifeCycleObserver;
 
   StreamSubscription<dynamic>? _eventSubscription;
 
@@ -125,10 +133,10 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
           isLoading: false,
           isFailedToLoad: false,
         );
-        // if (_initializedPosition != null) {
-        //   seekTo(_initializedPosition!.inSeconds);
-        // }
-        // if (_autoPlay) resume();
+        if (_initializedPosition != null) {
+          seekTo(_initializedPosition!.inSeconds);
+        }
+        if (_autoPlay) resume();
         break;
       case VideoEventType.resolutionUpdate:
         if (value.isStop) break;
@@ -162,7 +170,19 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     value = VideoPlayerValue.erroneous(e.message!);
   }
 
-  Future<void> setOnlineVideo({required String id, required String token}) {
+  Future<void> setOnlineVideo({
+    required String id,
+    required String token,
+    bool autoPlay = false,
+    Duration? position,
+  }) async {
+    _autoPlay = autoPlay;
+    _initializedPosition = position;
+    value = value.copyWith(
+      isReady: false,
+      isStop: false,
+      isLoading: true,
+    );
     return platform.setOnlineVideo(id: id, token: token);
   }
 
@@ -170,23 +190,54 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     return platform.play();
   }
 
-  Future<void> pause() {
-    return platform.pause();
+  Future<void> replay() async {
+    _initializedPosition = null;
+    await platform.play();
+    _autoPlay = true;
+    value = value.copyWith(
+      isReady: false,
+      isStop: false,
+      isLoading: true,
+    );
   }
 
-  Future<void> stop() {
-    return platform.stop();
+  Future<void> resume() async {
+    await platform.play();
+    value = value.copyWith(
+      isPlaying: true,
+    );
+  }
+
+  Future<void> pause() async {
+    await platform.pause();
+    value = value.copyWith(
+      isPlaying: false,
+    );
+  }
+
+  Future<void> stop() async {
+    await platform.stop();
+    value = value.copyWith(
+      isStop: true,
+      isPlaying: false,
+      isReady: false,
+    );
   }
 
   Future<void> seekTo(int position) {
+    value = value.copyWith(
+      position: Duration(seconds: position),
+    );
     return platform.seekTo(position);
   }
 
-  Future<void> setPlaybackSpeed(double speed) {
-    return platform.setPlaybackSpeed(speed);
+  Future<void> setPlaybackSpeed(double speed) async {
+    await platform.setPlaybackSpeed(speed);
+    value = value.copyWith(playbackSpeed: speed);
   }
 
   Future<void> setBackgroundPlay(bool backgroundPlay) {
+    _backgroundPlay = backgroundPlay;
     return platform.setBackgroundPlay(backgroundPlay);
   }
 
@@ -199,5 +250,37 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     // _lifeCycleObserver.dispose();
     _isDisposed = true;
     super.dispose();
+  }
+}
+
+class _VideoAppLifeCycleObserver extends Object with WidgetsBindingObserver {
+  _VideoAppLifeCycleObserver(this._controller);
+
+  bool _wasPlayingBeforePause = false;
+  final VideoPlayerController _controller;
+
+  void initialize() {
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (_controller.backgroundPlay) return;
+    switch (state) {
+      case AppLifecycleState.paused:
+        _wasPlayingBeforePause = _controller.value.isPlaying;
+        _controller.pause();
+        break;
+      case AppLifecycleState.resumed:
+        if (_wasPlayingBeforePause) {
+          _controller.resume();
+        }
+        break;
+      default:
+    }
+  }
+
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
   }
 }
